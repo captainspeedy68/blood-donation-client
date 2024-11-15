9c2e7f0c8bce1146171eb3666e7a9c71fd20095d
captainspeedy68 <bishwajitacharya303@gmail.com>
2024-11-16 01:36:01 +0600

    svg added and other small updates

--- src/app/(homepage)/Connecting.tsx
+++ src/app/(homepage)/Connecting.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import SearchDonor from "./SearchDonor";
+import SearchDonor from "../../components/SearchDonor";
 
 const Connecting = () => {
   return (
-    <div className="bg-[url('https://i.ibb.co.com/TrBjFC9/43d8ac7bb106e4c7c82a84cfa4ca4801.png')] bg-cover bg-center w-full">
-      <div className=" bg-white opacity-90">
-        <div className="flex justify-center p-8 items-center ">
-          <div className="flex-1 flex items-center justify-center">
+    <div className=" h-[1000px] bg-[url('https://i.ibb.co.com/TrBjFC9/43d8ac7bb106e4c7c82a84cfa4ca4801.png')]  bg-cover bg-center w-full">
+      <div className=" bg-white opacity-90 h-full">
+        <div className="flex justify-center pt-40 p-8 items-center ">
+          <div className=" flex-1 flex items-center justify-center">
             <div className="justify-center items-center align-middle lg:ml-10">
               <h1 className="font-normal text-6xl">
                 Connecting lives through blood donation
@@ -25,13 +25,16 @@ const Connecting = () => {
           </div>
           <div className="object-cover flex-1 flex justify-center rounded-lg">
             <img
-              className="w-[618px] rounded-2xl items-center h-[412px]  object-contain"
+              className="w-[618px] opacity-full rounded-2xl items-center h-[412px]  object-contain"
               src="https://i.postimg.cc/9FjvYFHH/e4a65d599ad47bb2d6d4874b2574aa43.png"
               alt=""
             />
           </div>
         </div>
         <div>
+          <div className="text-center font-normal text-2xl mb-12 mt-7">
+            Search Donor
+          </div>
           <SearchDonor />
         </div>
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
-import Heading from "@/app/(homepage)/Heading";
+import Heading from "@/components/Heading";
 import Image from "next/image";
 import Connecting from "./(homepage)/Connecting";
+import JoiningMission from "./(homepage)/JoiningMission";
 
 export default function Home() {
   return (
-    <div className=" min-h-screen pb-20">
+    <div className=" min-h-screen">
       <Heading></Heading>
       <Connecting></Connecting>
+      <JoiningMission></JoiningMission>
     </div>
   );
 }
