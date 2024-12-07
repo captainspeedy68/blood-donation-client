9f9caf7291359d575a9045064ec2ece562ad13d4
Bishal Acharya <82877265+captainspeedy68@users.noreply.github.com>
2024-12-07 22:35:08 +0600

    updating the handle search donors

---
+++ src/services/postApi.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+export const getDivisions = async () => {
+    const url = "http://localhost:3000/divisions-districts.json";
+    const res = await fetch(url);
+    const data = await res.json();
+    // console.log("API Response:", data); // Inspect the structure of the data
+    return data; // Ensure this matches the expected format
+  };
+
+  export const handleDonorSearch = async (
+    bloodGroup: string,
+    division: string,
+    district: string
+  ): Promise<any> => {
+    const url = "http://localhost:4000/api/donors/";
+  
+    const params = {
+      bloodGroup,
+      division,
+      district,
+    };
+  
+    try {
+      const response = await axios.get(url, { params }); // Include `params` in the request
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching donors:", error);
+      throw error;
+    }
+  };
+  
+  
